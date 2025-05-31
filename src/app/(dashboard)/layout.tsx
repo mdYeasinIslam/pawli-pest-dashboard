9ace28e71bc563113ec66a196a5f8637b5b93040
mdYeasinIslam <hyeasinislam@gmail.com>
2025-06-01 01:53:10 +0600

    project setup

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+
+
+
+export default function layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      {children}
+      {/* <Footer /> */}
+    </div>
+  );
+}
---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,11 @@
+import HomeSection from "@/components/Home/HomeSection";
+
+const page = () => {
+  return (
+    <div>
+      <HomeSection />
+    </div>
+  );
+};
+
+export default page;
